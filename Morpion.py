7ca111ad57edf013807a00ec99edfc3d34555671
tbrunetAkoyo <tbrunet@gaming.tech>
2022-11-24 21:48:07 +0100

    Projet

--- Morpion.py
+++ Morpion.py
@@ -1,12 +1,12 @@
 import random
-
+from random import randrange
 
 nom = input("veuillez saisir votre nom : ")
 
 
 def initGame():
     board = ["-", "-", "-",
-            "-", "-", "-",
+            "-", "-", "-", 
             "-", "-", "-"]
     currentPlayer = "X"
     gameRunning = True
@@ -97,39 +97,166 @@ def changement(currentPlayer):
 
 # IA
 def IA(board, currentPlayer):
-    while currentPlayer == "O":
-        position = random.randint (0,8)
-        if board[position] == "-" :
-            board[position] = "O"
-            currentPlayer = changement(currentPlayer)
+    id_win = verifCloseWin(board, currentPlayer)
+    real_player = changement(currentPlayer)
+    id_lose = verifCloseWin(board, real_player)
+    if id_win:
+        board[id_win] = currentPlayer
+    elif id_lose:
+        board[id_lose] = currentPlayer
+    else:
+        if verifCentre(board):
+            board[4] = currentPlayer
+        # elif vérification si un pion est déjà posé de l'ia. Si oui vérifié comme commenter en dessous
+        # si non entrer dans vérifCorner.
+        elif verifCorner(board):
+            not_put = 1
+            list_corner = [0, 2, 6, 8]
+            while not_put == 1:
+                random_position = list_corner.pop(randrange(len(list_corner)))
+                if board[random_position] == '-':
+                    board[random_position] = currentPlayer
+                    not_put = 0
+
+# Il va falloir regarder si tu as la possibilité de poser sur une diagonal où aucun pion enemi n'est posé.
+# Faire la même chose pour les verticales et horizontales
+
+# Privilégié les diagonales
+
+
+    currentPlayer = changement(currentPlayer)
+    # while currentPlayer == "O":
+    #     position = random.randint (0,8)
+    #     if board[position] == "-" :
+    #         board[position] = "O"
+    #         currentPlayer = changement(currentPlayer)
+
     return currentPlayer, board
 
+def verifCentre(board):
+    if board[4] == '-':
+        return True
+    return False
+
+def verifCorner(board):
+    if '-' in (board[0], board[2], board[6], board[8]):
+        return True
+    return False
+
+
+def verifDiagoWin(board, player):
+    diago1 = {0: board[0], 4: board[4], 8: board[8]}
+    diago1_list = [board[0], board[4], board[8]]
+
+    diago2 = {2: board[2], 4: board[4], 6: board[6]}
+    diago2_list = [board[2], board[4], board[6]]
+
+    if diago1_list.count(player) == 2:
+        for key, value in diago1.items():
+            if value == '-':
+                return key
+    elif diago2_list.count(player) == 2:
+        for k, v in diago2.items():
+            if v == '-':
+                return k
+
+def verifVerticalWin(board, player):
+    vertical1 = {0: board[0], 3: board[3], 6: board[6]}
+    vertical1_list = [board[0], board[3], board[6]]
+
+    vertical2 = {1: board[1], 4: board[4], 7: board[7]}
+    vertical2_list = [board[1], board[4], board[7]]
+
+    vertical3 = {2: board[2], 5: board[5], 8: board[8]}
+    vertical3_list = [board[2], board[5], board[8]]
+
+    if vertical1_list.count(player) == 2:
+        for k, v in vertical1.items():
+            if v == '-':
+                return k
+    elif vertical2_list.count(player) == 2:
+        for k, v in vertical2.items():
+            if v == '-':
+                return k
+    elif vertical3_list.count(player) == 2:
+        for k, v in vertical3.items():
+            if v == '-':
+                return k
+
+def verifHorizontalWin(board, player):
+    horizontal1 = {0: board[0], 1: board[1], 2: board[2]}
+    horizontal1_list = [board[0], board[1], board[2]]
+
+    horizontal2 = {3: board[3], 4: board[4], 5: board[5]}
+    horizontal2_list = [board[3], board[4], board[5]]
+
+    horizontal3 = {6: board[6], 7: board[7], 8: board[8]}
+    horizontal3_list = [board[6], board[7], board[8]]
+
+    print(horizontal1_list)
+    if horizontal1_list.count(player) == 2:
+        for k, v in horizontal1.items():
+            if v == '-':
+                return k
+    elif horizontal2_list.count(player) == 2:
+        for k, v in horizontal2.items():
+            if v == '-':
+                return k
+    elif horizontal3_list.count(player) == 2:
+        for k, v in horizontal3.items():
+            if v == '-':
+                return k
+
+
+def verifCloseWin(board, player):
+    id_diago = verifDiagoWin(board, player)
+    if id_diago:
+        return id_diago
+    id_vertical = verifVerticalWin(board, player)
+    if id_vertical:
+        return id_vertical
+    id_horizontal = verifHorizontalWin(board, player)
+    if id_horizontal:
+        return id_horizontal
+    
+    
+
+# vérification de nouvelle victoire ou égalité 
 
 def game():
     board, currentPlayer, gameRunning = initGame()
-    while gameRunning :
+    while gameRunning:
+        # Print board pour savoir où la partie en est.
+        # Pour que le joueur sache où il peut poser
         printBoard(board)
+        # Tour du player
         board = PlayerInput(board, currentPlayer)
         check = vérifWin(board)
         if check:
             gameRunning = False
+            printBoard(board)
             break
         check = égalité(board)
         if check:
             gameRunning = False
+            printBoard(board)
             break
+
+        # Changement de joueur à l'IA
         currentPlayer = changement(currentPlayer)
+
+        # Tour de l'IA et passage au joueur
         currentPlayer, board = IA(board, currentPlayer)
         check = vérifWin(board)
         if check:
             gameRunning = False
+            printBoard(board)
             break
     
-    rematch = input("voulez-vous rejouez ? (o, n)")
-
+    rematch = input("Voulez-vous rejouer ? (o, n)")
     if rematch.lower() == "o":
         game()
-    else :
-        print("Fin du jeu")
+    else:
+        print("Fin du jeu.")
 
 game()
